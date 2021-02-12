12a9beb3ed4b9b6c3b9b2861d85613a359b9e2a6
alexander <coding@aggrosoft.de>
2021-02-12 14:16:38 +0100

    add admin language

---
+++ Application/views/admin/de/agadcell_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$aLang = array(
+    'charset'                                   => 'UTF-8',
+    'SHOP_MODULE_GROUP_adcell_main' => 'Stamm',
+    'SHOP_MODULE_sEventId' => 'Event ID',
+    'HELP_SHOP_MODULE_sEventId' => 'Parameter <code>eventid</code> aus dem Adcell Tracking Code, e.g. <br/><br/><code>&lt;script type="text/javascript" src="https://t.adcell.com/t/track.js?pid=1234&eventid=<strong>4567</strong>&referenz=ABC_123&betrag=123.45" ...&gt;</code>',
+    'SHOP_MODULE_sProgramId' => 'Programm ID',
+    'HELP_SHOP_MODULE_sProgramId' => 'Parameter <code>pid</code> aus dem Adcell Tracking Code, e.g. <br/><br/><code>&lt;script type="text/javascript" src="https://t.adcell.com/t/track.js?pid=<strong>1234</strong>&eventid=4567&referenz=ABC_123&betrag=123.45" ...&gt;</code>',
+);
